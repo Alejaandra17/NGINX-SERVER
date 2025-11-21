ff6ca6ee78b83233d1d680e42f3b3132d8f20947
Alejaandra17 <alejaandra.fdez@gmail.com>
2025-11-21 17:51:07 +0100

    feat(docker): Add Nginx config, manual run script, and website content

---
+++ docker-server/docker-comand.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 1. Define Variables
+CONTAINER_NAME="nginx-docker-commands"
+HOST_PORT="8080"
+
+# 2. Run the container
+echo "Starting Nginx manually on port $HOST_PORT..."
+
+docker run -d \
+  --name $CONTAINER_NAME \
+  -p $HOST_PORT:80 \
+  -v "$(pwd)/html":/usr/share/nginx/html \
+  -v "$(pwd)/conf/nginx.conf":/etc/nginx/conf.d/default.conf \
+  nginx:latest
+
+echo "Done. Access at http://localhost:$HOST_PORT"
